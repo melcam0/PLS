99043d8315c40906b44cc65546d08724374e8760
Camillo Melzi <camillomelzi@gmail.com>
2024-04-17 18:14:43 +0200

    Prova per nuovo tipo installazione

--- PLS.vbs
+++
@@ -1,2 +0,0 @@
-Randomize
-CreateObject("Wscript.Shell").Run "R-Portable\App\R-Portable\bin\R.exe CMD BATCH --vanilla --slave runShinyApp.R" & " " & RND & " ", 0, False
--- runShinyApp.R
+++
@@ -1,7 +0,0 @@
-.libPaths("./R-Portable/App/R-Portable/library")
-## the path to portable chrome (da usare se ci sara bisogno in futuro)
-# browser.path = file.path(getwd(),"GoogleChromePortable/GoogleChromePortable.exe")
-# options(browser = browser.path)
-# shiny::runApp("./Shiny/",port=8888,launch.browser=TRUE)
-## al momento usiamo browser predefinito
-shiny::runApp("./Shiny/",launch.browser=TRUE)
